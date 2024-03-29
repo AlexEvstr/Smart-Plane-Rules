0dc12068fc98b266165b86cb8d7c338bbdde0c45
Alexander Evstratov <alexdrugev@me.com>
2024-03-29 14:07:37 +0300

    Add quiz a/an with correct or wrong result.

---
+++ Assets/_Scripts/PlaneDetector.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlaneDetector : MonoBehaviour
+{
+    [SerializeField] private GameObject _question;
+    [SerializeField] private TMP_Text _questionText;
+    [SerializeField] private TMP_Text _answer_1;
+    [SerializeField] private TMP_Text _answer_2;
+
+    [SerializeField] private string[] _questions;
+
+    [SerializeField] private GameObject _correct;
+    [SerializeField] private GameObject _wrong;
+
+    private List<string> _aArticle = new List<string>{ "Wolf", "Pen", "Child", "Spoon", "Plate", "Unicorn", "Mouse", "Ruler", "Teacher", "Boy", "Pencil", "Uniform", "Cherry", "Bus"}; 
+    private List<string> _anArticle = new List<string> { "Apple", "Hour", "Orange", "Elephant", "Umbrella", "Eye", "Airplane", "Onion", "Armchair" };
+
+    private void Start()
+    {
+        NewQuestion();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.transform.GetChild(0).GetComponent<TMP_Text>().text == "A")
+        {
+            if (_aArticle.Contains(_questionText.text))
+            {
+                StartCoroutine(ShowCorrect());
+            }
+            else
+            {
+                StartCoroutine(ShowWrong());
+            }
+
+            NewQuestion();
+        }
+        else if (collision.gameObject.transform.GetChild(0).GetComponent<TMP_Text>().text == "An")
+        {
+            if (_anArticle.Contains(_questionText.text))
+            {
+                StartCoroutine(ShowCorrect());
+            }
+            else
+            {
+                StartCoroutine(ShowWrong());
+            }
+
+            NewQuestion();
+        }
+    }
+
+    private void NewQuestion()
+    {
+        _question.transform.position = new Vector2(12, 0);
+        int questionIndex = Random.Range(0, _questions.Length);
+        _questionText.text = _questions[questionIndex];
+
+        int answerIndex = Random.Range(0, 2);
+        if (answerIndex == 0)
+        {
+            _answer_1.text = "A";
+            _answer_2.text = "An";
+        }
+        else
+        {
+            _answer_1.text = "An";
+            _answer_2.text = "A";
+        }
+    }
+
+    private IEnumerator ShowCorrect()
+    {
+        _correct.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        _correct.SetActive(true);
+        float aColor = 1f;
+        yield return new WaitForSeconds(0.5f);
+        while (_correct.GetComponent<Image>().color.a > 0)
+        {
+            _correct.GetComponent<Image>().color = new Color(1, 1, 1, aColor -= 0.01f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        _correct.SetActive(false);
+    }
+
+    private IEnumerator ShowWrong()
+    {
+        _wrong.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        _wrong.SetActive(true);
+        float aColor = 1f;
+        yield return new WaitForSeconds(0.5f);
+        while (_wrong.GetComponent<Image>().color.a > 0)
+        {
+            _wrong.GetComponent<Image>().color = new Color(1, 1, 1, aColor -= 0.01f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        _wrong.SetActive(false);
+    }
+
+
+}
---
+++ Assets/_Scripts/QuestionMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionMovement : MonoBehaviour
+{
+    private float _questionSpeed = 2.0f;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * _questionSpeed);
+        //Debug.Log(transform.position);
+    }
+}
